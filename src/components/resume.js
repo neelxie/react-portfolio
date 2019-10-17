38e2973fc5b92a957538386298e3c6d53ae0d5eb
Derrick Sekidde <kidricederek@gmail.com>
2019-10-17 09:26:02 +0300

    feat(Setup): Set Up project - Create basic boiler plate code for project [finishes #1234]

---
+++ src/components/resume.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class Resume extends Component{
+    render(){
+        return(
+            <div>Big Boy</div>
+        )
+    }
+}
+
+export default Resume;
