25367994f1653bb18516f398d7d3f5b03420124d
Derrick Sekidde <kidricederek@gmail.com>
2019-10-22 11:46:01 +0300

    Updates

---
+++ precache-manifest.dcca9ed5d32e9d4276deead6a77cb04a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95bafbc0fdaed7f49df5e7d05e9db82d",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "dfe18825ae5a8b8d6d1f",
+    "url": "/react-portfolio/static/css/2.04aae6c4.chunk.css"
+  },
+  {
+    "revision": "92088161aacfb54c9117",
+    "url": "/react-portfolio/static/css/main.01e91297.chunk.css"
+  },
+  {
+    "revision": "dfe18825ae5a8b8d6d1f",
+    "url": "/react-portfolio/static/js/2.e8918276.chunk.js"
+  },
+  {
+    "revision": "92088161aacfb54c9117",
+    "url": "/react-portfolio/static/js/main.5e2b1769.chunk.js"
+  },
+  {
+    "revision": "175a75cdfaf1231de18b",
+    "url": "/react-portfolio/static/js/runtime-main.98067e24.js"
+  },
+  {
+    "revision": "4cebdf42cdc580cbbc696c8b79a7578b",
+    "url": "/react-portfolio/static/media/bg.4cebdf42.jpg"
+  }
+]);
